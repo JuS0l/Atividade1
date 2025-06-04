7493d38749c02e14412d316f00666f43016e7650
Juliana e João <ju.s.oliveira23@gmail.com>
2025-06-04 08:39:29 -0300

    Criando código ProdutoRepositorio

--- Atividade1/Models/Produto.cs
+++ Atividade1/Models/Produto.cs
@@ -5,7 +5,7 @@
         public int IdProd { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
-        public decimal Preco { get; set; }
-        public decimal Quantidade { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
     }
 }
--- Atividade1/Repositorio/ProdutoRepositorio.cs
+++ Atividade1/Repositorio/ProdutoRepositorio.cs
@@ -1,10 +1,60 @@
 ﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Math.EC.Multiplier;
 using System.Data;
 
 
 namespace Atividade1.Repositorio
 {
-    public class ProdutoRepositorio
+    public class ProdutoRepositorio(IConfiguration Configuration)
     {
+        private readonly string _conexaoMySQL = Configuration.GetConnectionString("ConexaoMySQL");
+        public ProdutoRepositorio ObterProduto(string email)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new("SELECT * from Usuarios where Email = @email", conexao);
+                cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = email;
+
+                using (MySqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection)) 
+                {
+                    Produto produto = null;
+
+                    if(dr.Read())
+                    {
+                        produto = new Produto
+                        {
+                            Id = Convert.ToInt32(dr["IdProd"]),
+                            Nome = dr["Nome"].ToString(),
+                            Descricao = dr["Descricao"].ToString(),
+                            Preco = Convert.ToDouble (dr["Preco"]),
+                            Quantidade =  Convert.ToInt32 (dr["Quantidade"]),
+
+
+                        };
+                    }
+
+                    return produto;
+                }
+            }
+        }
+        public void AdcionarProduto(Produto produto)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new("INSERT INTO Produto(IdProd, Nome, Descricao, Preco, Quantidade) VALUES (@IdProd, @Nome, @Descricao, @Preco, @Quantidade)", conexao);
+                cmd.Parameters.AddWithValue("@IdProd", produto.Id);
+                cmd.Parameters.AddWithValue("@Nome", produto.Nome);
+                cmd.Parameters.AddWithValue("@Descricao", produto.Descricao);
+                cmd.Parameters.AddWithValue("@Preco", produto.Preco);
+                cmd.Parameters.AddWithValue("@Quantidade", produto.Quantidade);
+
+                cmd.ExecuteNonQuery();
+
+                conexao.Close();
+            }
+        }
     }
 }
